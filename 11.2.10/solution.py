2df847634863d42a8bbad944730fd851f9a6eeb3
Your Name <you@example.com>
2021-10-30 19:16:24 +0300

    11.2.10

---
+++ 11.2.10/solution.py
@@ -0,0 +1,18 @@
+"""
+
+import unittest
+"""
+
+
+
+class TestFactorization(unittest.TestCase):
+    def test_simple_multipliers(self):
+        x = 77
+        a, b = factorize(x)
+        self.assertEqual(a*b,x)
+"""
+def factorize(x):
+    return [7,10]
+
+unittest.main()
+"""
